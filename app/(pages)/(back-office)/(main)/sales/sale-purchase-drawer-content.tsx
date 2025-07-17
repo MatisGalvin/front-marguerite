1129fa0c828b63083a996f27c65ec1859728ee40
MatisGalvin <72927745+MatisGalvin@users.noreply.github.com>
2025-07-17 21:27:02 +0200

    Fix height SalePurchaseDrawerContent component

--- app/(pages)/(back-office)/(main)/sales/sale-purchase-drawer-content.tsx
+++ app/(pages)/(back-office)/(main)/sales/sale-purchase-drawer-content.tsx
@@ -1,7 +1,7 @@
-import * as React from "react"
-import { Minus, PanelBottomClose, Plus, X } from "lucide-react"
+import * as React from "react";
+import { Minus, PanelBottomClose, Plus, X } from "lucide-react";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Drawer,
   DrawerClose,
@@ -10,34 +10,36 @@ import {
   DrawerFooter,
   DrawerHeader,
   DrawerTitle,
-} from "@/components/ui/drawer"
-import { Sale } from "@/types/sale.type"
-import { toPrettyDate } from "@/lib/date-utils"
-import { PurchaseTable } from "@/features/purchase/purchase-table"
-import { SaleCard } from "./sale-card"
+} from "@/components/ui/drawer";
+import { Sale } from "@/types/sale.type";
+import { toPrettyDate } from "@/lib/date-utils";
+import { PurchaseTable } from "@/features/purchase/purchase-table";
+import { SaleCard } from "./sale-card";
 
-export function SalePurchaseDrawerContent(p: { sale?: Sale, onOpenChange: (isOpen: boolean) => void }) {
-
+export function SalePurchaseDrawerContent(p: {
+  sale?: Sale;
+  onOpenChange: (isOpen: boolean) => void;
+}) {
   if (!p.sale) {
-    return null
+    return null;
   }
 
   return (
     <Drawer open={!!p.sale} onOpenChange={p.onOpenChange}>
-      <DrawerContent className="p-0 m-0">
+      <DrawerContent className="m-0 h-screen p-0">
         <div>
           <div className="flex justify-end p-4">
-            <X className="cursor-pointer" onClick={() => p.onOpenChange(false)} />
+            <X
+              className="cursor-pointer"
+              onClick={() => p.onOpenChange(false)}
+            />
           </div>
           <div className="p-8">
             <SaleCard sale={p.sale} />
-            <PurchaseTable purchases={p.sale.purchases} className="h-[40rem] " />
+            <PurchaseTable purchases={p.sale.purchases} />
           </div>
-
-
-
         </div>
       </DrawerContent>
-    </Drawer >
-  )
+    </Drawer>
+  );
 }
