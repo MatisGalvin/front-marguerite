83892cdedcd6e0f935e721f7405f4e5eca356a73
MatisGalvin <72927745+MatisGalvin@users.noreply.github.com>
2025-08-03 12:49:45 +0200

    fix: refresh status on payment toggle

--- app/(pages)/(back-office)/(main)/purchases/page.tsx
+++ app/(pages)/(back-office)/(main)/purchases/page.tsx
@@ -86,6 +86,7 @@ export default function Purchases() {
           ),
         });
       }
+      queryClient.invalidateQueries({ queryKey: ["purchases"] });
     },
     onError: () => {
       if (shouldShowToastFeedbackRef.current == true) {
